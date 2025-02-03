92cee4ed19ceeb4bfad9fd14b7cb504d42e02b06
DrumkitReact <poojamtelang@gmail.com>
2025-02-03 12:16:02 +0530

    updated code

--- server/Controllers/quizController.js
+++ server/Controllers/quizController.js
@@ -14,3 +14,14 @@ exports.getQuestions = async (req, res) => {
       res.status(500).json({ message: 'Error fetching questions', error });
     }
   };
+
+
+  // exports.getQuestions = async (req, res) => {
+  //   try {
+  //     const questions = await Quiz.aggregate([{ $sample: { size: 10 } }, { $match: { examId: req.params.examId } }]); // Randomize 10 quizzes for the exam
+  //     res.status(200).json(questions);
+  //   } catch (error) {
+  //     res.status(500).json({ message: 'Failed to fetch quizzes' });
+  //   }
+  // };
+  
