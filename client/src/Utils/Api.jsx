ba207639a329b9cae7360434da362ab0079a2858
Pooja Telang <40686528+poojatelang@users.noreply.github.com>
2025-02-04 09:55:08 +0530

    Update Api.jsx

--- client/src/Utils/Api.jsx
+++ client/src/Utils/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base API URL
-const API_URL = "https://onlineexamination-eisb.onrender.com/api/";
+const API_URL = "https://onlineexamination-vc7t.onrender.com/api/";
 
 // Create Axios instance
 const api = axios.create({
