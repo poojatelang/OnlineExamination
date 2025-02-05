3d05eed5e2811ff5dedd177923157eaabdad94e3
Pooja Telang <40686528+poojatelang@users.noreply.github.com>
2025-02-05 22:00:40 +0530

    Update Api.jsx

--- client/src/Utils/Api.jsx
+++ client/src/Utils/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base API URL
-const API_URL = "http://localhost:3500/api/";
+const API_URL = "https://onlineexamination-daml.onrender.com/api/";
 
 // Create Axios instance
 const api = axios.create({
